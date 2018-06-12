b3c868845caa5c23097d19bab08f95e05bafc034
Elizabeth Gholson <eliz.gholson.9@gmail.com>
2018-06-12 03:03:09 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -15,9 +15,12 @@ function forLoop(array){
 
 let n = 100
 function whileLoop(n){
+  
   while (n > 0){
      console.log(--n);
+     
        if (n === 0){
+         
       return 'done';
       
     }
